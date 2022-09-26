674e2ed9a395be8dee197d5adb0055141fe4edb2
bravo3081472 <63444149+bravo3081472@users.noreply.github.com>
2022-09-26 13:16:47 +0800

    Update config

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,3 +2,8 @@
 module.exports = {
   NODE_ENV: '"production"'
 }
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  APIPATH: '"https://vue-course-api.hexschool.io"',
+  VUECAKE: '"vuecake"',
+});
